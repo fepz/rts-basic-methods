29bf954a6d6c36eebed00abfb452bc9041d6351c
fepz <franpaez+github@gmail.com>
2020-08-20 17:02:41 -0300

    Generate PDFs with solutions and excercises.

--- solver-tex.py
+++ solver-tex.py
@@ -1,30 +1,14 @@
 from pylatex import Document, Section, Subsection, Subsubsection, Command, Math, Package, Alignat
 from pylatex.basic import NewPage, LineBreak, NewLine
 from pylatex.utils import italic, NoEscape
-from pylatex.base_classes import Environment, Container
+from pylatex.base_classes import Environment, Container, Options
+from simso.generator import task_generator
 from functools import reduce
 import math
+import string
 import argparse
 import json
-
-
-rrts_list = {'a': [(1, 3, 3), (1, 4, 4), (1, 6, 6)],
-            'b': [(2, 4, 4), (1, 5, 5), (1, 7, 7)],
-            'c': [(2, 4, 4), (1, 5, 5), (1, 6, 6), (1, 12, 12)],
-            'd': [(2, 5, 5), (1, 7, 7), (2, 10, 10), (2, 17, 17)],
-            'e': [(2, 5, 5), (1, 8, 8), (2, 10, 10), (2, 15, 15)],
-            'f': [(2, 5, 5), (1, 6, 6), (1, 7, 7), (2, 10, 10), (2, 30, 30)],
-            'g': [(6, 40, 40), (5, 50, 50), (10, 60, 60), (12, 70, 70), (10, 80, 80)],
-            'h': [(10, 50, 50), (6, 60, 60), (8, 60, 60), (12, 80, 80), (7, 90, 90), (10, 100, 100)],
-            'i': [(1, 4, 4), (1, 6, 6), (1, 8, 8), (1, 10, 10), (1, 12, 12), (1, 20, 20), (1, 22, 22), (2, 24, 24)],
-            'j': [(7, 55, 55), (3, 66, 66), (2, 66, 66), (4, 66, 66), (1, 70, 70), (11, 77, 77), (3, 77, 77),
-                  (7, 105, 105), (6, 110, 110), (18, 154, 154)],
-            'k': [(1, 5, 5), (1, 6, 6), (1, 7, 7), (1, 10, 10), (1, 20, 20)],
-            'l': [(1, 5, 5), (2, 7, 7), (1, 10, 10), (3, 15, 15)],
-            'm': [(1, 4, 4), (2, 7, 7), (1, 9, 9), (1, 15, 15), (2, 20, 20)],
-            'n': [(1, 5, 5), (3, 8, 8), (1, 12, 12), (1, 15, 15), (2, 20, 20)],
-            'o': [(2, 5, 5), (1, 8, 8), (2, 12, 12), (2, 13, 13)],
-            'p': [(1, 4, 4), (2, 7, 7), (2, 11, 11), (1, 15, 15,)]}
+import os
 
 
 class Dmath(Environment):
@@ -78,6 +62,7 @@ def joseph_wcrt(rts, doc):
     wcrt = [0] * len(rts)
     schedulable = True
     wcrt[0] = rts[0]["c"]  # task 0 wcet
+    rts[0]["r"] = wcrt[0]
 
     with doc.create(Subsubsection("Tarea 1")):
         doc.append(Math(data=["R_1=C_1={:d}".format(wcrt[0])], escape=False))
@@ -137,6 +122,7 @@ def rta_wcrt(rts, doc):
     wcrt = [0] * len(rts)
     schedulable = True
     wcrt[0] = rts[0]["c"]  # task 0 wcet
+    rts[0]["r"] = wcrt[0]
 
     with doc.create(Subsubsection("Tarea 1")):
         doc.append(Math(data=["R_1=C_1={:d}".format(wcrt[0])], escape=False))
@@ -147,22 +133,28 @@ def rta_wcrt(rts, doc):
         cc = 0
         iter = 0
         with doc.create(Subsubsection("Tarea {0:}".format(i+1))):
-            doc.append(Math(data=["t^0=R_{0:}+C_{1:}={2:}".format(i, i+1, r)], escape=False))
+            doc.append(Math(data=["t^0=R_{{ {0:} }}+C_{{ {1:} }}={2:}".format(i, i+1, r)], escape=False))
             while schedulable:
                 iter += 1
                 w = 0
-                l = ["{0:}".format(c)]
-                l2 = ["t^{0:}=".format(iter)]
                 for taskp in rts[:i]:
                     cp, tp = taskp["c"], taskp["t"]
                     w += math.ceil(float(r) / float(tp)) * cp
                     cc += 1
-                    l.append("\\ceil*{\\frac{" + str(r) + '}{' + str(tp) + "}} }} {:0}".format(cp))
-                l2.extend(['+'.join(map(str, l))])
                 w = c + w
+
+                # Latex
+                l2 = ["t^{0:}={1:}+".format(iter, c)]
+                l2.extend(['+'.join(map(str,
+                                        ["\\ceil*{{\\frac{{ {0:} }} {{ {1:} }} }} {2:}".format(r, task["t"], task["c"]) for task in rts[:i]]
+                                        ))])
                 l2.append("={:0}".format(w))
-                l2.append("=t^{0:} \Rightarrow R_{1:}=t^{2:}={3:}".format(iter-1, i+1, iter, r) if r == w else "\\neq t^{0:}".format(iter-1))
+                if w <= d:
+                    l2.append("=t^{0:} \Rightarrow R_{{ {1:} }}=t^{{ {2:} }}={3:}".format(iter - 1, i + 1, iter, r) if r == w else "\\neq t^{{ {0:} }}".format(iter - 1))
+                else:
+                    l2.append(">D_{{ {0:} }}".format(i+1))
                 doc.append(Math(data=l2, escape=False))
+
                 if r == w:
                     break
                 r = w
@@ -185,13 +177,13 @@ def first_free_slot(rts, doc):
     for i, task in enumerate(rts, 0):
         r = task["r"] + task["c"] if i > 0 else task["c"]
         with doc.create(Subsubsection("Tarea {0:}".format(i+1))):
-            data = ["t^0=R_{0:}+C_{1:}={2:}".format(i, i + 1, r)] if i > 0 else ["t^0=C_1={0:}".format(r)]
+            data = ["t^0=R_{{ {0:} }}+C_{{ {1:} }}={2:}".format(i, i + 1, r)] if i > 0 else ["t^0=C_1={{ {0:} }}".format(r)]
             doc.append(Math(data=data, escape=False))
             iter = 0
             while True:
                 iter += 1
                 l = ["1"]
-                l2 = ["t^{0:}=".format(iter)]
+                l2 = ["t^{{ {0:} }}=".format(iter)]
                 w = 0
                 for taskp in rts[:i+1]:
                     c, t = taskp["c"], taskp["t"]
@@ -200,7 +192,7 @@ def first_free_slot(rts, doc):
                 w = w + 1
                 l2.extend(['+'.join(map(str, l))])
                 l2.append("={:0}".format(w))
-                l2.append("=t^{0:}".format(iter-1) if r == w else "\\neq t^{0:}".format(iter-1))
+                l2.append("=t^{{ {0:} }}".format(iter-1) if r == w else "\\neq t^{{ {0:} }}".format(iter-1))
                 doc.append(Math(data=l2, escape=False))
                 if r == w:
                     break
@@ -229,12 +221,12 @@ def calculate_k(rts, doc):
         with doc.create(Subsubsection("Tarea {0:}".format(i+1))):
             iter = 0
             doc.append("Con ")
-            doc.append(Math(data=["K_{0:} = {1:}".format(i + 1, k)], escape=False, inline=True))
+            doc.append(Math(data=["K_{{ {0:} }} = {1:}".format(i + 1, k)], escape=False, inline=True))
             while True:
                 iter += 1
                 w = 0
                 l = ["{0:}".format(k)]
-                l2 = ["t^{0:}=".format(iter)]
+                l2 = ["t^{{ {0:} }}=".format(iter)]
                 for taskp in rts[:i]:
                     cp, tp = taskp["c"], taskp["t"]
                     w += math.ceil(float(r) / float(tp)) * cp
@@ -243,36 +235,137 @@ def calculate_k(rts, doc):
                 l2.extend(['+'.join(map(str, l))])
                 l2.append("={:0}".format(w))
                 if w <= d:
-                    l2.append("=t^{0:}".format(iter-1) if r == w else "\\neq t^{0:}".format(iter-1))
+                    l2.append("=t^{{ {0:} }}".format(iter-1) if r == w else "\\neq t^{{ {0:} }}".format(iter-1))
                 else:
-                    l2.append(">D_{0:}".format(i+1))
+                    l2.append(">D_{{ {0:} }}".format(i+1))
                 doc.append(Math(data=l2, escape=False))
                 if r == w:
                     r = 1
                     k = k + 1
                     doc.append("Con ")
-                    doc.append(Math(data=["K_{0:} = {1:}".format(i + 1, k)], escape=False, inline=True))
+                    doc.append(Math(data=["K_{{ {0:} }} = {1:}".format(i + 1, k)], escape=False, inline=True))
                 r = w
                 if r > d:
                     break
             ks[i] = k - 1
             task["k"] = k - 1
             doc.append("El máximo retraso para la tarea {0:} es ".format(i+1))
-            doc.append(Math(data=["K_{0:}={1:}".format(i+1, task["k"])], escape=False, inline=True))
+            doc.append(Math(data=["K_{{ {0:} }}={1:}".format(i+1, task["k"])], escape=False, inline=True))
             doc.append(".")
     return ks
 
 
-def add_rts(key, rts, doc):
+def wcrt(rts):
+    """ Calcula el WCRT de cada tarea del str y evalua la planificabilidad """
+    wcrt = [0] * len(rts)
+    schedulable = True
+    wcrt[0] = rts[0]["c"]  # task 0 wcet
+    for i, task in enumerate(rts[1:], 1):
+        c, t, d = task["c"], task["t"], task["d"]
+        r = wcrt[i - 1] + c
+        while schedulable:
+            w = 0
+            for taskp in rts[:i]:
+                cp, tp = taskp["c"], taskp["t"]
+                w += math.ceil(float(r) / float(tp)) * cp
+            w = c + w
+            if r == w:
+                break
+            r = w
+            if r > d:
+                schedulable = False
+        wcrt[i] = r
+        if not schedulable:
+            break
+    return [schedulable, wcrt]
 
+
+def gen_rts(params):
+
+    rts_list = []
+
+    for param in params:
+        ntask, uf, mint, maxt = param["ntask"], param["uf"], param["mint"], param["maxt"]
+
+        print("Generating RTS with params {0:}".format(param))
+
+        sched_found = False
+        while not sched_found:
+            u = task_generator.gen_randfixedsum(1, ntask, uf)
+            t = task_generator.gen_periods_uniform(ntask, 1, mint, maxt, round_to_int=True)
+            rts = []
+            for taskset in iter(task_generator.gen_tasksets(u, t)):
+                for task in taskset:
+                    c, t = task
+                    rts.append({"c": math.ceil(c), "t": int(t), "d": int(t)})
+            rts = sorted(rts, key=lambda k: k['t'])
+            sched_found = wcrt(rts)[0]
+            if sched_found:
+                rts_list.append(rts)
+
+    return rts_list
+
+
+def add_rts_2(key, rts, doc):
+    def free_slots(rts):
+        """ Calcula primer instante que contiene un slot libre por subsistema """
+        free = [0] * len(rts)
+        for i, task in enumerate(rts, 0):
+            r = 1
+            while True:
+                w = 0
+                for taskp in rts[:i + 1]:
+                    c, t = taskp["c"], taskp["t"]
+                    w += math.ceil(float(r) / float(t)) * float(c)
+                w = w + 1
+                if r == w:
+                    break
+                r = w
+            free[i] = r
+        return free
+
+    def calc_k(rts):
+        """ Calcula el K de cada tarea (maximo retraso en el instante critico) """
+        ks = [0] * len(rts)
+        ks[0] = rts[0]["d"] - rts[0]["c"]
+
+        for i, task in enumerate(rts[1:], 1):
+            r = 1
+            k = 1
+            c, t, d = task["c"], task["t"], task["d"]
+            while True:
+                w = 0
+                for taskp in rts[:i]:
+                    cp, tp = taskp["c"], taskp["t"]
+                    w += math.ceil(float(r) / float(tp)) * cp
+                w = c + w + k
+                if r == w:
+                    r = 1
+                    k = k + 1
+                r = w
+                if r > d:
+                    break
+            ks[i] = k - 1
+        return ks
+
+    with doc.create(Section("STR " + key)):
+        doc.append(Math(data=["\Gamma("+str(len(rts))+")", '=', "\\left\\{",
+                              ",".join("\\left({0:}, {1:}, {2:}\\right)".format(task["c"], task["t"], task["d"]) for task in rts),
+                              "\\right\\}"], escape=False))
+        doc.append(Math(data=[",".join(["R_{{ {0:} }}={1:}".format(i, r) for i, r in enumerate(wcrt(rts)[1], 1)])], escape=False))
+        doc.append(Math(data=[",".join(["K_{{ {0:} }}={1:}".format(i, r) for i, r in enumerate(calc_k(rts), 1)])], escape=False))
+        doc.append(Math(data=[",".join(["F_{{ {0:} }}=\\left[{1:},{2:}\\right]".format(i, int(r)-1, int(r)) for i, r in enumerate(free_slots(rts), 1)])], escape=False))
+
+
+def add_rts(key, rts, doc):
     rts_uf = uf(rts)
 
     with doc.create(Section('STR ' + str(key))):
         ",".join("({0:}, {1:}, {2:}".format(task["c"], task["t"], task["d"]) for task in rts)
 
-        doc.append(Math(data=["\Gamma("+str(len(rts))+")", '=', '\{',
+        doc.append(Math(data=["\Gamma("+str(len(rts))+")", '=', "\\left\\{",
                               ",".join("({0:}, {1:}, {2:})".format(task["c"], task["t"], task["d"]) for task in rts),
-                              '\}'], escape=False))
+                              "\\right\\}"], escape=False))
 
         doc.append(Math(data=['H=', str(lcm(rts))]))
 
@@ -283,7 +376,7 @@ def add_rts(key, rts, doc):
                 s.append("\\frac{" + str(task["c"]) + '}{' + str(task["t"]) + '}')
             a.extend(['+'.join(map(str, s)), '=', "{:.3f}".format(uf(rts))])
             doc.append(Math(data=a, escape=False))
-            doc.append("El FU del sistema es de {:.0%}.".format(uf(rts)))
+            doc.append("El FU del sistema es de {:.00%}.".format(uf(rts)))
 
         with doc.create(Subsection('Cota de Liu')):
             liu = liu_bound(rts)
@@ -297,7 +390,7 @@ def add_rts(key, rts, doc):
         with doc.create(Subsection('Cota de Bini')):
             bini = bini_bound(rts)
             with doc.create(Dmath()):
-                a = ["\prod_{i=1}^{n}".replace('n', str(len(rts))), "\\left(\\frac{C_i}{T_i}-1\\right)="]
+                a = ["\prod_{i=1}^{n}".replace('n', str(len(rts))), "\\left(\\frac{C_i}{T_i}+1\\right)="]
                 s = []
                 for task in rts:
                     s.append("\\left(\\frac{"+str(task["c"])+'}{'+str(task["t"])+'}+1\\right)')
@@ -334,44 +427,76 @@ def add_rts(key, rts, doc):
 def getargs():
     """ Command line arguments """
     parser = argparse.ArgumentParser(description="Basic methods for RTS schedulability and wcrt analysis.")
-    parser.add_argument("rts", nargs="*", help="RTS to evaluate")
+    parser.add_argument("--params-file", type=argparse.FileType('r'), help="JSON file with RTS params.")
+    parser.add_argument("--rts-file", type=argparse.FileType('r'), help="JSON file with RTS.")
+    parser.add_argument("--rts", nargs="*", help="RTS to evaluate")
+    parser.add_argument("--pdf-name", type=str, help="Name of the output PDF file(s). If topic is greater than one, it's appended to the filename.")
+    parser.add_argument("--topics", type=int, default=1, help="Number of topics.")
     return parser.parse_args()
 
 
-def main():
-    args = getargs()
+def generate_pdf(rts_list, pdf_name, topic=0):
+    geometry_options = {"tmargin": "2cm", "lmargin": "2cm", "rmargin": "2cm", "bmargin": "2cm"}
 
-    geometry_options = {"tmargin": "2cm", "lmargin": "2cm", "rmargin":"2cm", "bmargin":"2cm"}
+    doc = Document(fontenc="T1", inputenc="utf8", geometry_options=geometry_options, document_options="fleqn")
+    doc2 = Document(fontenc="T1", inputenc="utf8", geometry_options=geometry_options, document_options="fleqn")
 
-    doc = Document('results', fontenc="T1", inputenc="utf8", geometry_options=geometry_options, document_options="fleqn")
-
-    # Packages
-    doc.packages.append(Package('amssymb'))
-    doc.packages.append(Package('bookmark'))
-    doc.packages.append(Package('mathtools'))
+    packages = [
+        Package('hyperref', Options("pdftex", pdfauthor="IF025", pdftitle="TP1")),
+        Package('amssymb'),
+        Package('bookmark'),
+        Package('mathtools')
+    ]
+    for package in packages:
+        doc.packages.append(package)
+        doc2.packages.append(package)
 
     # Use \ceil to enclose expressions
     doc.preamble.append(NoEscape(r'\DeclarePairedDelimiter{\ceil}{\lceil}{\rceil}'))
     doc.preamble.append(NoEscape(r'\DeclarePairedDelimiter{\floor}{\lceil}{\floor}'))
 
-    doc.preamble.append(Command('title', 'Trabajo Práctico Nro. 1'))
-    doc.preamble.append(Command('date', NoEscape(r'\today')))
+    doc.preamble.append(Command('title', "Trabajo Práctico Nro. 1 - Tema {0:}".format(topic)))
+    doc.preamble.append(Command('date', NoEscape(r'\the\year{}')))
     doc.append(NoEscape(r'\maketitle'))
 
-    with open("rts.json") as file:
-        rts_list = json.load(file)
+    doc2.preamble.append(Command('title', "Trabajo Práctico Nro. 1 - Tema {0:}".format(topic)))
+    doc2.preamble.append(Command('date', NoEscape(r'\the\year{}')))
+    doc2.append(NoEscape(r'\maketitle'))
 
-        if not args.rts:
-            l = sorted(rts_list)
-        else:
-            l = args.rts
+    for k, rts in zip(string.ascii_lowercase, gen_rts(rts_list)):
+        add_rts(k, rts, doc)
+        add_rts_2(k, rts, doc2)
+    doc.generate_pdf(filepath=pdf_name + "-soluciones")
+    doc2.generate_pdf(filepath=pdf_name)
 
-        for k in l:
-            rts = rts_list[k]
-            add_rts(k, rts, doc)
 
-        doc.generate_tex()
-        doc.generate_pdf()
+def main():
+    args = getargs()
+
+    if args.rts_file:
+        with args.rts_file as file:
+            rts_list = json.load(file)
+
+            if not args.rts:
+                l = sorted(rts_list)
+            else:
+                l = args.rts
+
+            for k in l:
+                rts = rts_list[k]
+                #add_rts(k, rts, doc)
+
+            #doc.generate_tex()
+            #doc.generate_pdf()
+
+    if args.params_file:
+        with args.params_file as file:
+            params_list = json.load(file)
+            for topic in range(1, args.topics+1):
+                print("Topic {0:} ...".format(topic))
+                filepath = "{0:}-tema-{1:}".format(args.pdf_name if args.pdf_name else os.path.splitext(file.name)[0], topic)
+                generate_pdf(params_list, filepath, topic=topic)
+            print("Done!")
 
 
 if __name__ == '__main__':
