e556b234c09d8427f2b09792ce2e5819cc1df45c
fep <franpaez@gmail.com>
2025-11-22 02:27:21 -0300

    Soporte para nuevos formatos
    Mediante files.py se soportan nuevos formatos de archivo.

--- solver.py
+++ solver.py
@@ -1,19 +1,20 @@
-import json
 import argparse
+import sys
 from functools import reduce
 from math import ceil, gcd
 from simso.generator import task_generator
 from tabulate import tabulate
+from files import get_from_file
 
 
 def lcm(rts):
     """ Real-time system hiperperiod (l.c.m) """
-    return reduce(lambda x, y: (x * y) // gcd(x, y), [task["t"] for task in rts], 1)
+    return reduce(lambda x, y: (x * y) // gcd(x, y), [task["T"] for task in rts], 1)
 
 
 def uf(rts):
     """ Real-time system utilization factor """
-    return sum([float(task["c"]) / float(task["t"]) for task in rts])
+    return sum([float(task["C"]) / float(task["T"]) for task in rts])
 
 
 def round_robin(rts):
@@ -21,9 +22,9 @@ def round_robin(rts):
     min_d = float("inf")
     sum_c = 0
     for task in rts:
-        if task["d"] < min_d:
-            min_d = task["d"]
-        sum_c = sum_c + task["c"]
+        if task["D"] < min_d:
+            min_d = task["D"]
+        sum_c = sum_c + task["C"]
     return [min_d >= sum_c, min_d, sum_c]
 
 
@@ -35,7 +36,7 @@ def liu_bound(rts):
 
 def bini_bound(rts):
     """ Evaluate schedulability using the hyperbolic bound """
-    bound = reduce(lambda a, b: a*b, [float(task["c"]) / float(task["t"]) + 1 for task in rts])
+    bound = reduce(lambda a, b: a*b, [float(task["C"]) / float(task["T"]) + 1 for task in rts])
     return [bound, bound <= 2.0]
 
 
@@ -43,15 +44,15 @@ def joseph_wcrt(rts):
     """ Evaluate schedulability using the Joseph & Pandya exact schedulability test """
     wcrt = [0] * len(rts)
     schedulable = True
-    wcrt[0] = rts[0]["c"]  # task 0 wcet
+    wcrt[0] = rts[0]["C"]  # task 0 wcet
     for i, task in enumerate(rts[1:], 1):
         t = 0
         while schedulable:
-            w = task["c"] + sum([ceil(float(t) / float(taskp["t"]))*taskp["c"] for taskp in rts[:i]])
+            w = task["C"] + sum([ceil(float(t) / float(taskp["T"]))*taskp["C"] for taskp in rts[:i]])
             if t == w:
                 break
             t = w
-            if t > task["d"]:
+            if t > task["D"]:
                 schedulable = False
         wcrt[i] = t
         if not schedulable:
@@ -63,15 +64,15 @@ def rta_wcrt(rts):
     """ Calcula el WCRT de cada tarea del str y evalua la planificabilidad """
     wcrt = [0] * len(rts)
     schedulable = True
-    wcrt[0] = rts[0]["c"]  # task 0 wcet
+    wcrt[0] = rts[0]["C"]  # task 0 wcet
     for i, task in enumerate(rts[1:], 1):
-        r = wcrt[i-1] + task["c"]
+        r = wcrt[i-1] + task["C"]
         while schedulable:
-            w = task["c"] + sum([ceil(float(r) / float(taskp["t"]))*taskp["c"] for taskp in rts[:i]])
+            w = task["C"] + sum([ceil(float(r) / float(taskp["T"]))*taskp["C"] for taskp in rts[:i]])
             if r == w:
                 break
             r = w
-            if r > task["d"]:
+            if r > task["D"]:
                 schedulable = False
         wcrt[i] = r
         if not schedulable:
@@ -90,7 +91,7 @@ def first_free_slot(rts):
     for i, task in enumerate(rts, 0):
         t = 0
         while True:
-            w = 1 + sum([ceil(float(t) / float(taskp["t"]))*taskp["c"] for taskp in rts[:i+1]])
+            w = 1 + sum([ceil(float(t) / float(taskp["T"]))*taskp["C"] for taskp in rts[:i+1]])
             if t == w:
                 break
             t = w
@@ -101,13 +102,13 @@ def first_free_slot(rts):
 def calculate_k(rts):
     """ Calcula el K de cada tarea (maximo retraso en el instante critico) """
     ks = [0] * len(rts)
-    ks[0] = rts[0]["t"] - rts[0]["c"]
+    ks[0] = rts[0]["T"] - rts[0]["C"]
 
     for i, task in enumerate(rts[1:], 1):
         t = 0
         k = 1
-        while t <= task["d"]:
-            w = k + task["c"] + sum([ceil(float(t) / float(taskp["t"]))*taskp["c"] for taskp in rts[:i]])
+        while t <= task["D"]:
+            w = k + task["C"] + sum([ceil(float(t) / float(taskp["T"]))*taskp["C"] for taskp in rts[:i]])
             if t == w:
                 k += 1
             t = w
@@ -119,20 +120,20 @@ def calculate_y(rts):
     wcrt = rta_wcrt(rts)[1]
     y = []
     for i, task in enumerate(zip(rts, wcrt), 1):
-        y.append(task[0]["d"] - task[1])
+        y.append(task[0]["D"] - task[1])
     return y
 
 
 def calculate_ps_bound(rts):
     u = uf(rts)
     bound = (len(rts)+1) * (pow(2, 1.0 / float(len(rts)+1)) - 1)
-    return [((bound - u) * task["t"], task["t"]) for task in rts]
+    return [((bound - u) * task["T"], task["T"]) for task in rts]
 
 
 def calculate_ds_bound(rts):
     p = pow((uf(rts) / len(rts)) + 1, len(rts))
     uds = (2 - p) / ((2 * p) - 1)
-    return [(uds * task["t"], task["t"]) for task in rts]
+    return [(uds * task["T"], task["T"]) for task in rts]
 
 
 def calculate_ds_k(rts):
@@ -141,8 +142,8 @@ def calculate_ds_k(rts):
         return float(k) / (float(ceil(float(t) / float(tds))))
     ks = calculate_k(rts)
     cds_list = []
-    for tds in [task["t"] for task in rts]:
-        cds_list.append((min([f(k, task["t"], tds) for k, task in zip(ks, rts)]), tds))
+    for tds in [task["T"] for task in rts]:
+        cds_list.append((min([f(k, task["T"], tds) for k, task in zip(ks, rts)]), tds))
     return cds_list
 
 
@@ -165,7 +166,7 @@ def generate_rts(param):
         for taskset in iter(task_generator.gen_tasksets(u, t)):
             for task in taskset:
                 c, t = task
-                rts.append({"c": ceil(c), "t": int(t), "d": int(t)})
+                rts.append({"C": ceil(c), "T": int(t), "D": int(t)})
         rts = sorted(rts, key=lambda k: k['t'])
         if wcrt(rts)[0]:
             return rts
@@ -174,9 +175,11 @@ def generate_rts(param):
 def getargs():
     """ Command line arguments """
     parser = argparse.ArgumentParser(description="Basic methods for RTS schedulability and WCRT analysis.")
-    parser.add_argument("file", type=argparse.FileType('r'), help="JSON file with RTS or RTS params.")
-    parser.add_argument("--rts", type=str, help="RTS to evaluate")
+    parser.add_argument("file", type=argparse.FileType('r'), default=sys.stdin, help="JSON file with RTS or RTS params.")
+    parser.add_argument("--rts", type=str, default="0", help="RTS to evaluate")
     parser.add_argument("--table", action="store_true", default=False)
+    parser.add_argument("--print-rts", action="store_true", default=False)
+    parser.add_argument("--only-print-rts", action="store_true", default=False)
     return parser.parse_args()
 
 
@@ -184,37 +187,44 @@ def main():
     args = getargs()
 
     with args.file as file:
-        rts_in_file = json.load(file)
-        for rts in [rts_in_file[i] for i in mix_range(args.rts)] if args.rts else rts_in_file:
-            if type(rts) is list:
-                for task in rts:
-                    if "d" not in task:
-                        task["d"] = task["t"]
-            if type(rts) is dict:
-                rts = generate_rts(rts)
+        #rts_in_file = json.load(file)
+        #for rts in [rts_in_file[i] for i in mix_range(args.rts)] if args.rts else rts_in_file:
+        for rts in get_from_file(file, mix_range(args.rts)):
+            ptasks = rts["ptasks"]
 
-            print("RTS: {0:}".format(rts))
+            if args.print_rts or args.only_print_rts:
+                column_names = ptasks[0].keys()
+                print(tabulate(ptasks, headers="keys", tablefmt="simple"))
+
+            if args.only_print_rts:
+                continue
+
+            rm_schedulable = rta_wcrt(ptasks)[0]
+
+            liu_bound_result = liu_bound(ptasks)
+            bini_bound_result = bini_bound(ptasks)
+
             results = [
-                ("h", lcm(rts)),
-                ("uf", uf(rts)),
-                ("liu", liu_bound(rts)),
-                ("bini", bini_bound(rts)),
-                ("wcrt", wcrt(rts)),
-                ("edf", (uf(rts) <= 1)),
-                ("free", first_free_slot(rts)),
-                ("k", calculate_k(rts)),
-                ("y", calculate_y(rts)),
-                ("rr", round_robin(rts)),
-                ("ps (bound)", calculate_ps_bound(rts)),
-                ("ds (bound)", calculate_ds_bound(rts)),
-                ("ds (k)", calculate_ds_k(rts))
+                ("h", lcm(ptasks)),
+                ("uf", uf(ptasks)),
+                ("liu", liu_bound_result),
+                ("bini", bini_bound_result),
+                ("wcrt", wcrt(ptasks)),
+                ("edf", (uf(ptasks) <= 1)),
+                ("free", first_free_slot(ptasks) if rm_schedulable else "No planificable"),
+                ("k", calculate_k(ptasks)),
+                ("y", calculate_y(ptasks)),
+                ("rr", round_robin(ptasks)),
+                ("ps (bound)", calculate_ps_bound(ptasks) if liu_bound_result[1] else "No aplica"),
+                ("ds (bound)", calculate_ds_bound(ptasks) if bini_bound_result[1] else "No aplica"),
+                ("ds (k)", calculate_ds_k(ptasks))
             ]
             if args.table:
                 print(tabulate(results, tablefmt="grid"))
             else:
                 for result in results:
-                    k, v = result
-                    print("{}\t{}".format(k, v))
+                    key, value = result
+                    print(f"{key}\t{value}")
 
 
 if __name__ == '__main__':
